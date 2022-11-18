ec3f9eb63dd4b9eef176988226ce537db0ae66c9
jinran-google <jinran-google@users.noreply.github.com>
2022-11-18 10:17:41 +0000

    deploy: ce53f450a8ff04eaa18e1d7faf477a2f71ac3fe4

--- search/classes_a.js
+++ search/classes_a.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['oncecallback_853',['OnceCallback',['../d3/d2f/classotbr_1_1OnceCallback.html',1,'otbr']]],
-  ['oncecallback_3c_20r_28args_2e_2e_2e_29_3e_854',['OnceCallback&lt; R(Args...)&gt;',['../d3/d7b/classotbr_1_1OnceCallback_3_01R_07Args_8_8_8_08_4.html',1,'otbr']]],
-  ['oncecallback_3c_20void_28otbrerror_20aerror_29_3e_855',['OnceCallback&lt; void(otbrError aError)&gt;',['../d3/d2f/classotbr_1_1OnceCallback.html',1,'otbr']]],
-  ['onmeshprefix_856',['OnMeshPrefix',['../d5/d02/structotbr_1_1DBus_1_1OnMeshPrefix.html',1,'otbr::DBus']]],
-  ['openthreadclient_857',['OpenThreadClient',['../d3/d3f/classotbr_1_1Web_1_1OpenThreadClient.html',1,'otbr::Web']]],
-  ['optional_858',['Optional',['../de/dac/classOptional.html',1,'']]]
+  ['oncecallback_934',['OnceCallback',['../d3/d2f/classotbr_1_1OnceCallback.html',1,'otbr']]],
+  ['oncecallback_3c_20r_28args_2e_2e_2e_29_3e_935',['OnceCallback&lt; R(Args...)&gt;',['../d3/d7b/classotbr_1_1OnceCallback_3_01R_07Args_8_8_8_08_4.html',1,'otbr']]],
+  ['oncecallback_3c_20void_28otbrerror_20aerror_29_3e_936',['OnceCallback&lt; void(otbrError aError)&gt;',['../d3/d2f/classotbr_1_1OnceCallback.html',1,'otbr']]],
+  ['onmeshprefix_937',['OnMeshPrefix',['../d5/d02/structotbr_1_1DBus_1_1OnMeshPrefix.html',1,'otbr::DBus']]],
+  ['openthreadclient_938',['OpenThreadClient',['../d3/d3f/classotbr_1_1Web_1_1OpenThreadClient.html',1,'otbr::Web']]],
+  ['optional_939',['Optional',['../de/dac/classOptional.html',1,'']]]
 ];
--- search/classes_f.js
+++ search/classes_f.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['ubusagent_882',['UBusAgent',['../df/d00/classotbr_1_1ubus_1_1UBusAgent.html',1,'otbr::ubus']]],
-  ['ubusserver_883',['UbusServer',['../d9/dea/classotbr_1_1ubus_1_1UbusServer.html',1,'otbr::ubus']]]
+  ['ubusagent_964',['UBusAgent',['../df/d00/classotbr_1_1ubus_1_1UBusAgent.html',1,'otbr::ubus']]],
+  ['ubusserver_965',['UbusServer',['../d9/dea/classotbr_1_1ubus_1_1UbusServer.html',1,'otbr::ubus']]]
 ];
